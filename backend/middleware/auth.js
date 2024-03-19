d1ef2fe1910ab085b0643a1b69b5a2c8c1776387
Janindu Gayanga <83686809+jani358@users.noreply.github.com>
2024-03-19 16:24:18 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,27 +1,30 @@
+const jwt = require("jsonwebtoken"); // Importing the jsonwebtoken library
+const User = require("../models/User"); // Importing the User model
 
-const jwt = require("jsonwebtoken")
-const User = require("../models/User")
-
-exports.isAutheticated = async (req,res,next) => {
+// Middleware to check if user is authenticated
+exports.isAuthenticated = async (req, res, next) => {
     try {
-        const {token} = req.cookies//assign cookie in token
-        if(!token){//toeken is empty that mean user is'nt login
-            return res.status(401).json({//send error msg
-                message:"Please Login First"
-            })
+        const { token } = req.cookies; // Extracting the token from request cookies
+        
+        // If token is not present, user is not logged in
+        if (!token) {
+            return res.status(401).json({
+                message: "Please Login First"
+            });
         }
-    
-        const decode = jwt.verify(token, process.env.JWT_SECRET) //decode token and verify using secret
-//decode token and verify using secret
-        req.user = await User.findById(decode._id)//assing token id that mean user id. we assign that genarate token method
-    
-    
-        next()//go to next work post/delete etc.....
-    } catch (error) {//send error
+        
+        // Verifying and decoding the token using the secret
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        
+        // Finding the user based on the decoded user ID from the token
+        req.user = await User.findById(decoded._id);
+        
+        // Proceed to the next middleware or route handler
+        next();
+    } catch (error) {
+        // If any error occurs, send a 500 Internal Server Error response
         res.status(500).json({
-            message:error.message
-        })
+            message: error.message
+        });
     }
-   
-  
-}
+};
